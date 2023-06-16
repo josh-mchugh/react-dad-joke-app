e00584e9dbc00abbf69a9ba396184ceb86243175
Josh McHugh <joshua.mchugh01@gmail.com>
2023-06-16 00:12:50 -0400

    Cleaning up some classes and element structure

--- src/App.jsx
+++ src/App.jsx
@@ -18,17 +18,22 @@ function App() {
     return (
         <>
           <div className="bg-gray-700 h-screen flex items-center">
-            <div className="mx-auto max-w-md text-center mb-24">
+            <div className="mx-auto max-w-md text-center flex flex-col gap-y-6 mb-24">
               <div>
-                <h2 className="text-3xl fond-bold tracking-tight text-white sm:text-4xl">Lets hear some dad joke!</h2>
+                <h2 className="text-3xl fond-bold tracking-tight text-white sm:text-4xl">
+                  Lets hear some dad joke!
+                </h2>
               </div>
               <div>
                 <PunchLine joke={joke}/>
               </div>
               <div>
-                <div className="mt-10 flex items-center justify-center gap-x-6">
-                  <button className="rounded-md bg-white px-3.5 py-2.5 text-sm font-semibold text-gray-900 shadow-sm hover:bg-gray-100 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-white" onClick={() => handleFetchJokeClick()}>Fetch Joke</button>
-                </div>
+                <button
+                  className="rounded-md bg-white px-3.5 py-2.5 text-sm font-semibold text-gray-900 shadow-sm hover:bg-gray-100 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-white"
+                  onClick={() => handleFetchJokeClick()}
+                >
+                  Fetch Joke
+                </button>
               </div>
             </div>
           </div>
