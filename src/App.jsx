adfcb2d7d9e3d4e5ab0d2039257314844e2e136d
Josh McHugh <joshua.mchugh01@gmail.com>
2023-06-17 10:35:13 -0400

    Created a Header component

--- src/App.jsx
+++ src/App.jsx
@@ -21,26 +21,30 @@ function App() {
           <div className="bg-slate-900 h-screen flex items-center">
             <div className="mx-auto max-w-xl mb-24">
               <div className="flex flex-col gap-y-6 px-6 py-6 bg-slate-800 text-slate-200 text-center border border-slate-600 rounded-lg shadow">
-
-                <div>
-                  <h2 className="text-3xl fond-bold tracking-tight">
-                  Lets hear some dad jokes!
-                  </h2>
-                </div>
+                <Header/>
                 <PunchLine joke={joke}/>
-                <Button onClick={handleFetchJoke}/>
+                <Button onClick={handleFetchJoke} hasJoke={!!joke}/>
               </div>
             </div>
           </div>
         </>
   );
-};
+}
+
+function Header(props) {
+    return (
+        <div>
+          <h2 className="text-3xl fond-bold tracking-tight">
+            Lets hear some dad jokes!
+          </h2>
+        </div>
+    );
+}
 
 function PunchLine(props) {
     PunchLine.propTypes = {
         joke: PropTypes.string
     };
-
     return (
         <>
           <div>
@@ -54,7 +58,11 @@ function PunchLine(props) {
 
 function Button(props) {
     Button.propTypes = {
+        hasJoke: PropTypes.bool.isRequired,
         onClick: PropTypes.func.isRequired
+    };
+    const getButtonText = () => {
+        return props.hasJoke ? 'Fetch Another Joke' : 'Fetch Joke';
     };
     return (
         <>
@@ -63,7 +71,7 @@ function Button(props) {
               type="button"
               onClick={props.onClick}
               className="border focus:ring-4 focus:outline-none font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 border-slate-600 hover:text-white hover:bg-slate-600 focus:ring-slate-800">
-              Fetch Joke
+              { getButtonText() }
             </button>
           </div>
         </>
