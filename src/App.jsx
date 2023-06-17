e10d5cbf36528122e10331b101ca81289f6b2689
Josh McHugh <joshua.mchugh01@gmail.com>
2023-06-17 01:11:34 -0400

    Cleaning up some styles and creating components

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import PropTypes from 'prop-types';
 function App() {
     const [joke, setJoke] = useState('');
 
-    const handleFetchJokeClick = () => {
+    const handleFetchJoke = () => {
 
         fetch('https://icanhazdadjoke.com/', {
               headers: {
@@ -18,29 +18,23 @@ function App() {
 
     return (
         <>
-          <div className="bg-gray-700 h-screen flex items-center">
-            <div className="mx-auto max-w-md text-center flex flex-col gap-y-6 mb-24">
-              <div>
-                <h2 className="text-3xl fond-bold tracking-tight text-white sm:text-4xl">
-                  Lets hear some dad joke!
-                </h2>
-              </div>
-              <div>
+          <div className="bg-slate-900 h-screen flex items-center">
+            <div className="mx-auto max-w-xl mb-24">
+              <div className="flex flex-col gap-y-6 px-6 py-6 bg-slate-800 text-center border border-gray-200 rounded-lg shadow">
+
+                <div>
+                  <h2 className="text-3xl fond-bold tracking-tight text-white sm:text-4xl">
+                  Lets hear some dad jokes!
+                  </h2>
+                </div>
                 <PunchLine joke={joke}/>
-              </div>
-              <div>
-                <button
-                  className="rounded-md bg-white px-3.5 py-2.5 text-sm font-semibold text-gray-900 shadow-sm hover:bg-gray-100 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-white"
-                  onClick={() => handleFetchJokeClick()}
-                >
-                  Fetch Joke
-                </button>
+                <Button onClick={handleFetchJoke}/>
               </div>
             </div>
           </div>
         </>
   );
-}
+};
 
 function PunchLine(props) {
     PunchLine.propTypes = {
@@ -49,12 +43,33 @@ function PunchLine(props) {
 
     return (
         <>
-          <p className="text-lg text-white">
-            {props.joke}
-          </p>
+          <div>
+            <p className="text-lg text-white">
+              {props.joke}
+            </p>
+          </div>
         </>
     );
-
 }
+
+function Button(props) {
+    Button.propTypes = {
+        onClick: PropTypes.func.isRequired
+    };
+    return (
+        <>
+          <div>
+            <button
+              type="button"
+              onClick={props.onClick}
+              className="text-gray-900 hover:text-white border border-gray-800 hover:bg-gray-900 focus:ring-4 focus:outline-none focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 dark:border-slate-600 dark:text-slate-200 dark:hover:text-white dark:hover:bg-slate-600 dark:focus:ring-slate-800">
+              Fetch Joke
+            </button>
+          </div>
+        </>
+    );
+}
+
+
 
 export default App;
